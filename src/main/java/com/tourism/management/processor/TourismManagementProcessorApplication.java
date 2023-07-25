0ed563085c750e8c2225f369c271345c7833f1b9
parthibhann <parthibhan.nagarajan@gmail.com>
2023-07-25 23:29:52 +0530

    inital commit

---
+++ src/main/java/com/tourism/management/processor/TourismManagementProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.tourism.management.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismManagementProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismManagementProcessorApplication.class, args);
+	}
+
+}
